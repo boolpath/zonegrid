9ce478e547aca336475965da875b4c4d4e439c8a
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-18 14:38:12 -0500

    Basic module structure and license

---
+++ zonegrid.js
@@ -0,0 +1,26 @@
+/* NODE MODULES */
+var eventerface = require('eventerface');
+
+/** LOCAL OBJECT 
+ * @property {} - 
+ */
+var ZONEGRID = {
+    
+};
+
+/** MODULE INTERFACE
+ *@method {function} - 
+ */
+module.exports = {
+    
+};
+
+/*----------------------------------------------------------------------------*/
+
+/** 
+ * @param
+ * @returns
+ */
+function () {
+    
+}
