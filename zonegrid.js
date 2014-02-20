f4d53e6176ae5cd9bd85342ae9cf1aff715f7d09
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-20 00:49:41 -0500

    Included the defaults module

--- zonegrid.js
+++ zonegrid.js
@@ -1,5 +1,8 @@
 /* NODE MODULES */
-var eventerface = require('eventerface');
+var eventerface = require('eventerface'),
+    zone = require('./zone'),
+    grid = require('./grid')
+    defaults = require('./defaults');
 
 /** LOCAL OBJECT 
  * @property {} - 
@@ -9,18 +12,44 @@ var ZONEGRID = {
 };
 
 /** MODULE INTERFACE
- *@method {function} - 
+ *@method {function} createZone - Creates a zone object after validating the specified properties
+ *@method {function} createGrid - Creates a grid object after validating the specified properties
  */
 module.exports = {
-    
+    createZone: createZone,
+    createGrid: createGrid
 };
 
 /*----------------------------------------------------------------------------*/
 
-/** 
- * @param
- * @returns
+/** Creates a zone object after validating the specified properties
+ * @param {object} properties - The properties of the zone to be created
+ * @returns {object} newZone - The created zone object
  */
-function () {
-    
+function createZone(properties) {
+    var newZone;
+
+    if (typeof properties !== 'object') {
+        properties = defaults.zoneProperties();
+    } 
+    else {
+    // Validate parameters
+
+    }
+
+    newZone = zone.create(properties);
+
+    return newZone;
+}
+
+/** Creates a grid object after validating the specified properties
+ * @param {object} properties - The properties of the grid to be created
+ * @returns {object} newGrid - The created grid object
+ */
+function createGrid(properties) {
+    var newGrid;
+
+    newGrid = grid.create(properties);
+
+    return newGrid;
 }
