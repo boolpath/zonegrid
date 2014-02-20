004f343cf8de6c1304c191e19098659c91a64b99
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-20 00:34:50 -0500

    Added the 'defaults' module for returning the default values of major objects

---
+++ defaults/defaults.js
@@ -0,0 +1,89 @@
+/** MODULE INTERFACE
+ *@method {function} - 
+ */
+module.exports = {
+    zoneProperties: defaultZoneProperties
+};
+
+/*----------------------------------------------------------------------------*/
+
+/** 
+ * @param
+ * @returns
+ */
+function defaultZoneProperties() {
+    return {
+        size: {
+            x: 1000,
+            y: 1000,
+            z: 0
+        },
+        visibility: {
+            horizontal: 100,
+            vertical: 100
+        },
+        handover: 50,
+        margins: {
+            scope: {
+                inner: {
+                    x: {
+                        lower: 100,
+                        higher: 900
+                    },
+                    y: {   
+                        lower: 100,
+                        higher: 900
+                    },
+                    z: {
+                        lower: 0,
+                        higher: 100
+                    }
+                },
+                outer: {
+                    x: {
+                        lower: -100,
+                        higher: 1100
+                    },
+                    y: {   
+                        lower: -100,
+                        higher: 1100
+                    },
+                    z: {
+                        lower: 0,
+                        higher: 100
+                    }
+                }
+            },
+            handover: {
+                bookin: {
+                    x: {
+                        lower: 50,
+                        higher: 950
+                    },
+                    y: {   
+                        lower: 50,
+                        higher: 950
+                    },
+                    z: {
+                        lower: 0,
+                        higher: 0
+                    }
+                },
+                checkin: {
+                    x: {
+                        lower: -50,
+                        higher: 1050
+                    },
+                    y: {   
+                        lower: -50,
+                        higher: 1050
+                    },
+                    z: {
+                        lower: 0,
+                        higher: 0
+                    }
+                }
+            }
+        }
+    }
+}
