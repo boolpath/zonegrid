43f0b0df6c8ce42e304d0bdc5bdf076dedceee7d
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-24 02:53:26 -0500

    Changed the neighbors model and exposed each neighbor through a server property whose #set operations are watch and evented

--- zone/neighbors/zoneNeighbors.js
+++ zone/neighbors/zoneNeighbors.js
@@ -9,44 +9,52 @@ module.exports = {
 
 /** Creates an object containing all the possible neighbor relationships of a zone
  * @param {object} neighbors - 
+ * @param {function} changeGetterSetter - 
  * @returns {object} neighborRelationships - 
  */
-function createRelationships(neighbors) {
-    var x = {}, y = {}, z = {}, defaultNeighbor = undefined;
-
-    if (typeof neighbors === 'object') {
-        x.lower  = (typeof neighbors.x === 'object') ? neighbors.x.lower  : defaultNeighbor;
-        x.higher = (typeof neighbors.x === 'object') ? neighbors.x.higher : defaultNeighbor;
-        y.lower  = (typeof neighbors.y === 'object') ? neighbors.z.lower  : defaultNeighbor;
-        y.higher = (typeof neighbors.y === 'object') ? neighbors.y.higher : defaultNeighbor;
-        z.lower  = (typeof neighbors.z === 'object') ? neighbors.z.lower  : defaultNeighbor;
-        z.higher = (typeof neighbors.z === 'object') ? neighbors.z.higher : defaultNeighbor;
-    } else {
-        x.lower = x.higher = defaultNeighbor;
-        y.higher = y.lower = defaultNeighbor;
-        z.higher = z.lower = defaultNeighbor;
-    }
-
-    function createNeighbor(value) {
+function createRelationships(neighbors, changeGetterSetter) {
+    function createNeighbor(location) {
+        var neighbor = neighbors[location] || { port: 0 };
+        neighbor = changeGetterSetter('neighbor', location, neighbor);
+        // return neighbor;
         return {
-            writable: true,
-            value: value
-        }
+            value: Object.create({}, {
+                server: neighbor
+            })
+        };
     }
 
     var neighborRelationships = {
-        x: Object.create({}, {
-            lower: createNeighbor(x.lower),
-            higher: createNeighbor(x.higher),
-        }),
-        y: Object.create({}, {
-            lower: createNeighbor(y.lower),
-            higher: createNeighbor(y.higher),
-        }),
-        z: Object.create({}, {
-            lower: createNeighbor(z.lower),
-            higher: createNeighbor(z.higher),
-        })
+        // XY plane, lower Z
+        'xl-yl-zl': createNeighbor('xl-yl-zl'),
+        'xl-ym-zl': createNeighbor('xl-ym-zl'),
+        'xl-yh-zl': createNeighbor('xl-yh-zl'),
+        'xm-yl-zl': createNeighbor('xm-yl-zl'),
+        'xm-ym-zl': createNeighbor('xm-ym-zl'),
+        'xm-yh-zl': createNeighbor('xm-yh-zl'),
+        'xh-yl-zl': createNeighbor('xh-yl-zl'),
+        'xh-ym-zl': createNeighbor('xh-ym-zl'),
+        'xh-yh-zl': createNeighbor('xh-yh-zl'),
+        // XY plane, middle Z
+        'xl-yl-zm': createNeighbor('xl-yl-zm'),
+        'xl-ym-zm': createNeighbor('xl-ym-zm'),
+        'xl-yh-zm': createNeighbor('xl-yh-zm'),
+        'xm-yl-zm': createNeighbor('xm-yl-zm'),
+        // 'xm-ym-zm': not included because it is the zone itself
+        'xm-yh-zm': createNeighbor('xm-yh-zm'),
+        'xh-yl-zm': createNeighbor('xh-yl-zm'),
+        'xh-ym-zm': createNeighbor('xh-ym-zm'),
+        'xh-yh-zm': createNeighbor('xh-yh-zm'),
+        // XY plane, higher Z
+        'xl-yl-zh': createNeighbor('xl-yl-zh'),
+        'xl-ym-zh': createNeighbor('xl-ym-zh'),
+        'xl-yh-zh': createNeighbor('xl-yh-zh'),
+        'xm-yl-zh': createNeighbor('xm-yl-zh'),
+        'xm-ym-zh': createNeighbor('xm-ym-zh'),
+        'xm-yh-zh': createNeighbor('xm-yh-zh'),
+        'xh-yl-zh': createNeighbor('xh-yl-zh'),
+        'xh-ym-zh': createNeighbor('xh-ym-zh'),
+        'xh-yh-zh': createNeighbor('xh-yh-zh'),
     };
 
     return neighborRelationships;
