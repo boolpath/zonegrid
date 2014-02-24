7c3ecbdb57abe5c35ec63c392f939c612d2c07bd
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-23 22:14:17 -0500

    Modified the scope margins when the visibility of an axis is changed

--- zone/events/zoneEvents.js
+++ zone/events/zoneEvents.js
@@ -24,24 +24,35 @@ function watchProperties(zone, properties) {
     properties.on('coordinatesChange', function (change) {
         var property = change.property,
             value = change.value,
+            visibility = zone.visibility[property],
+            handover = zone.handover,
             lowerLimit, higherLimit;
 
         lowerLimit = zone.limits[property].lower += value;
         higherLimit = zone.limits[property].higher += value;
         
-        zone.margins.scope.inner[property].lower  = lowerLimit  + zone.visibility[property];
-        zone.margins.scope.inner[property].higher = higherLimit - zone.visibility[property];
-        zone.margins.scope.outer[property].lower  = lowerLimit  - zone.visibility[property];
-        zone.margins.scope.outer[property].higher = higherLimit + zone.visibility[property];
+        zone.margins.scope.inner[property].lower  = lowerLimit  + visibility;
+        zone.margins.scope.inner[property].higher = higherLimit - visibility;
+        zone.margins.scope.outer[property].lower  = lowerLimit  - visibility;
+        zone.margins.scope.outer[property].higher = higherLimit + visibility;
 
-        zone.margins.handover.bookin[property].lower   = lowerLimit  + zone.handover;
-        zone.margins.handover.bookin[property].higher  = higherLimit - zone.handover;
-        zone.margins.handover.checkin[property].lower  = lowerLimit  - zone.handover;
-        zone.margins.handover.checkin[property].higher = higherLimit + zone.handover;
+        zone.margins.handover.bookin[property].lower   = lowerLimit  + handover;
+        zone.margins.handover.bookin[property].higher  = higherLimit - handover;
+        zone.margins.handover.checkin[property].lower  = lowerLimit  - handover;
+        zone.margins.handover.checkin[property].higher = higherLimit + handover;
 
     });
     properties.on('visibilityChange', function (change) {
         // console.log('visibilityChange');
+        var property = change.property,
+            visibility = change.value,
+            lowerLimit = zone.limits[property].lower;
+            higherLimit = zone.limits[property].higher;
+
+        zone.margins.scope.inner[property].lower  = lowerLimit  + visibility;
+        zone.margins.scope.inner[property].higher = higherLimit - visibility;
+        zone.margins.scope.outer[property].lower  = lowerLimit  - visibility;
+        zone.margins.scope.outer[property].higher = higherLimit + visibility;
     });
     properties.on('handoverChange', function (change) {
         // console.log('handoverChange');
