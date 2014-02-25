6b97c06e485c097d5bbe6d7fd81a1c9e74937d4a
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-25 18:40:33 -0500

    Added a jamp property to the servers object and a default port number

--- defaults/defaults.js
+++ defaults/defaults.js
@@ -148,6 +148,9 @@ function defaultZoneProperties() {
             'xh-yh-zh': null,
         },
         servers: {
+            jamp: {
+                port: 7070
+            }
             webServer: {
                 port: 8080
             },
